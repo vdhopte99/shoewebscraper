8be3392fbd0701b1c6ae946fe342701a42d898e2
vdhopteprinceton <vdhopte@princeton.edu>
2021-01-15 13:56:46 -0500

    scrape shoes

--- webscraper.py
+++ webscraper.py
@@ -4,5 +4,15 @@ PATH = "/Users/bigvdhopte/Documents/Vedant/Python/ShoeWebScraper/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://sneakernews.com/release-dates/")
-print(driver.title)
-driver.close()
+
+shoes = driver.find_elements_by_class_name("image-box")
+
+print(len(shoes))
+
+for shoe in shoes:
+    print(shoe.text)
+    print("---------------------------------------------------------------------------------------")
+
+driver.quit()
+
+
