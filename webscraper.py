7c15dee214ae33273223e543b81c23a6191045e8
vdhopteprinceton <vdhopte@princeton.edu>
2021-01-16 14:00:52 -0500

    breakthrough

--- webscraper.py
+++ webscraper.py
@@ -1,31 +1,14 @@
-# from selenium import webdriver
 import requests
 import urllib.request
-
-
-# from urllib.request import urlopen as uReq
+import sys
 from bs4 import BeautifulSoup as soup
-# from selenium.webdriver.common.keys import Keys
-# import time
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 
-# PATH = "/Users/bigvdhopte/Documents/Vedant/Python/ShoeWebScraper/chromedriver"
-# driver = webdriver.Chrome(PATH)
+headers={"User-Agent": 
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
+
 soleCollector = "https://solecollector.com/sneaker-release-dates/all-release-dates/"
-
 response = requests.get(soleCollector)
-
 sole_soup = soup(response.content, "html.parser")
-
-
-
-# uClient = uReq(soleCollector)
-# sole_html = uClient.read()
-# uClient.close()
-
-# sole_soup = soup(sole_html, "html.parser")
 shoes = sole_soup.findAll("div", {"class": "row"})
 januaryShoes = []
 
@@ -47,59 +30,31 @@ for shoe in shoes:
 
     januaryShoes.append((name, price))
 
-    # driver.get("https://stockx.com/")
-
-    # search = driver.find_element_by_name("q")
-
-    
-    # search.send_keys(januaryShoes[0][0])
-    # search.send_keys(Keys.RETURN)
-
-    # time.sleep(5)
-
-
-    # image = driver.find_element_by_class_name("tile browse-tile updated")
-    # image = image.find_element_by_tag_name("img")
-    # image = image.get_attribute("src")
-
-
-
-
-    # driver.quit()
 
 
 query = januaryShoes[0][0]
 query = query.split(" ")
 
-url = "https://stockx.com/search/sneakers?s="
+url = "https://www.google.com/search?sxsrf=ALeKk03vlxoCHnl3qL59fhmKl08krVu-Mg%3A1610822395327&ei=-zIDYL24E5il5NoP48Sg2Ak&q="
 
 for i in range(len(query)):
-    url += query[i]
-    if i != len(query) - 1:
-        url += "%20"
+    url += query[i] + "+"
 
-# driver.get(url)
-# time.sleep(3)
-# image = driver.find_element_by_class_name("price-line-div")
-# # image = image.find_element_by_tag_name("img")
-# # image = image.get_attribute("src")
-# driver.close()
+url += "stockx&oq=Adidas+Harden+Vol.+5+Crystal+White%2FCloud+White%2FRoyal+Blue+stockx&gs_lcp=CgZwc3ktYWIQAzoECCMQJzoHCCMQrgIQJzoFCCEQoAE6BAghEApQ7RhYiR9gwx9oAHAAeACAAXqIAd8FkgEDNi4ymAEAoAEBqgEHZ3dzLXdpesABAQ&sclient=psy-ab&ved=0ahUKEwj9oa2FjaHuAhWYElkFHWMiCJsQ4dUDCA0&uact=5"
 
-# print(image.text)
+response = requests.get(url, headers=headers)
+google_soup = soup(response.content, "html.parser")
 
+link = google_soup.find('div', {"class": "g"})
+link = link.find('a', href=True)
+link = link["href"]
 
-# # url = "https://stockx.com/search/sneakers?s=air%20force%201"
-
-response = requests.get(url)
+response = requests.get(link, headers=headers)
 stockx_soup = soup(response.content, "html.parser")
 
-# listings = stockx_soup.findAll("div", {"class": "price-line-div"})
-print(stockx_soup.prettify())
+price = stockx_soup.find("div", {"class": "stats"})
 
-
-print(len(listings))
-
-
+print(price.text)
 
 
 
